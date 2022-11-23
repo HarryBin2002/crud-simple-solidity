87ce8c9a1db497c903cc3deae86122edb8125066
harrybin <hoangmanhbinh2002@gmail.com>
2022-11-23 14:14:52 +0700

    updating web3js

--- client/index.js
+++ client/index.js
@@ -1,31 +1,27 @@
 const Web3 = require('web3');
-const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
 
-const abi = require('./build/CRUD.json');
-// const contractAddress = '0x55eDCB6627582404AD8a941a018b7e7C8A7b4461';
-const contractAddress = '0xc40Bd1b8710c5804AeA1b7f208DabD603111aF6e';
+const CRUD = require('./build/CRUD.json');
 
+const init = async () => {
+    const web3 = new Web3 ('http://127.0.0.1:9545/');
 
+    const contract = await new web3.eth.Contract(CRUD.abi, '0x7c04e1D5a89cDa9B19b0c4ac5ddA6a6F8B1457a4');
 
-const contract = new web3.eth.Contract(abi.abi, contractAddress);
+    const create  = await contract.methods.Create('harrybin').send({from: '0x6e34a8f6afd40890d1830872e1eb12330d07261a', gas: '1000000'});
 
+    // const balance = await contract.methods.totalPlayer().call();
+    // console.log(balance);
 
-// encoded = contract.methods.Create('harrybin').encodeABI()
-// var block = web3.eth.getBlock("latest");
-// var gasLimit = 20000000;
-
-// var tx = {
-//     gas: gasLimit,
-//     to: '0xc40Bd1b8710c5804AeA1b7f208DabD603111aF6e',
-//     data: encoded
-// }
-
-// web3.eth.accounts.signTransaction(tx, 'dbf48d914c013b586ec11b9e9a2ed1a19912aaf1a312c110c15b1b576cebcc3f').then(signed => {
-//     web3.eth.sendSignedTransaction(signed.rawTransaction).on('receipt', console.log)
-// })
+    const name1 = await contract.methods.getNameFromId(1).call();
+    console.log(name1);
+    
+    const deletePlayer = await contract.methods.Delete(2).send({from: '0x6e34a8f6afd40890d1830872e1eb12330d07261a', gas: '1000000'});
+    const balance = await contract.methods.totalPlayer().call();
+    console.log(balance);
+    const show = await contract.methods.showPlayer().call();
+    console.log(show);
+}
 
 
-contract.methods.Create('binh').send({from: '0xc40Bd1b8710c5804AeA1b7f208DabD603111aF6e'});
 
-// console.log( contract.methods.Read(1));
-
+init();
